55f9036500d35332e09ee00014a3d4fa6fbfb155
Andre Pan <apan@mingyian.com>
2018-03-26 04:08:46 +0800

    添加同时解析热词和热文的方法

--- utils/wx.parser.js
+++ utils/wx.parser.js
@@ -1,39 +1,54 @@
 const url = require('url')
 const domParser = require('cheerio')
 
-function hotWords(data, result, cb) {
+function hotWords(data, response, cb) {
+	response.data.hotwords = []
 	let $ = domParser.load(data)
 	$('#topwords a').each((i,e) => {
 		let keyword = $(e).text()
-		result.data[result.data.length] = {
-			"keyword": keyword
+		let popularity = $(e).next().find('span').attr('style').split(':')[1]
+		response.data.hotwords[response.data.hotwords.length] = {
+			"keyword": keyword,
+			"popularity": popularity
 		}
 	})
-	cb(result)
+	cb(response)
 }
 
-function articleList(data, result, cb) {
+function articleList(data, response, cb) {
+	response.data.articles = []
 	let $ = domParser.load(data)
 	$('ul.news-list li').each((i,e) => {
 		let titleObj = $(e).find('.txt-box h3 a')
 		let link = titleObj.attr('href')
 		let title = titleObj.text()
+		let desc = $(e).find('.txt-info').text()
 		let author = $(e).find('.s-p .account').text()
 		let imgObj = $(e).find('.img-box img').length > 0 ? $(e).find('.img-box img') : $(e).find('.txt-box .img-d img').eq(0)
-		let img = imgObj.attr('src')
+		let img = decodeURIComponent(imgObj.attr('src').split('url=')[1])
 		let query = url.parse(link,true).query
-		result.data[result.data.length] = {
+		response.data.articles[response.data.articles.length] = {
 			"title": title,
+			"description": desc,
 			"author": author,
 			"link": link,
 			"image": img,
 			"query": query
 		}
 	})
-	cb(result)
+	cb(response)
+}
+
+function all(data, response, cb) {
+	hotWords(data, response, interim_res => {
+		articleList(data, interim_res, res => {
+			cb(res)
+		})
+	})
 }
 
 module.exports = {
 	articleList: articleList,
-	hotWords: hotWords
+	hotWords: hotWords,
+	all: all
 }
